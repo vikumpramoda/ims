a4115e911f177d12bd5e83baabf4ed40a9b53c99
vikumpramoda <66731366+vikumpramoda@users.noreply.github.com>
2022-01-31 09:55:52 +0530

    Dashboard
    State Timber Corporation Analysing  Dashboard

--- modules/dashboard/index.php
+++ modules/dashboard/index.php
@@ -1,15 +1,31 @@
 <?php
 @session_start();
 include('db_connector.php');
-// dashboard 
+// dashboard vikum 2022-01-27
+$member_id  = $_SESSION['member_id'];
 
+$query = "SELECT * FROM member;";
+$result = mysqli_query($con, $query);
+$total_members=$result->num_rows;
+
+$query = "SELECT * FROM supplier;";
+$result2 = mysqli_query($con, $query);
+$total_supplier=$result2->num_rows;
+
+$query = "SELECT * FROM sub_cato;";
+$result3 = mysqli_query($con, $query);
+$total_sub_cato=$result3->num_rows;
+
+$query = "SELECT * FROM main_stock_item;";
+$result4 = mysqli_query($con, $query);
+$total_main_stock_item=$result4->num_rows;
 
 ?>
 
 	<main class="app-content">
       <div class="app-title">
         <div>
-            <h1><i class="fa fa-dashboard"></i> <b>Dashboard</b></h1>
+            <h1><a href="dashboard.php"><img class="circular--square" src="upload\logo3.jpg" alt="logo" /> <b>State Timber Corporation Inventory Management System</b></a> </h1>
 
         </div>
         <ul class="app-breadcrumb breadcrumb">
@@ -21,37 +37,37 @@ include('db_connector.php');
         <div class="col-md-6 col-lg-3">
           <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
             <div class="info">
-              <h4>Users</h4>
-              <p><b>5</b></p>
+              <h4>Member count</h4>
+              <p><b><?=$total_members;?></b></p>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+          <div class="widget-small info coloured-icon"><i class="icon fa fa-truck fa-3x"></i>
             <div class="info">
-              <h4>Likes</h4>
-              <p><b>25</b></p>
+              <h4>Supplier Count</h4>
+              <p><b><?=$total_supplier;?></b></p>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
+          <div class="widget-small warning coloured-icon"><i class="icon fa fa-sitemap fa-3x"></i>
             <div class="info">
-              <h4>Uploades</h4>
-              <p><b>10</b></p>
+              <h4>S. Category Count</h4>
+              <p><b><?=$total_sub_cato;?></b></p>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small danger coloured-icon"><i class="icon fa fa-star fa-3x"></i>
+          <div class="widget-small danger coloured-icon"><i class="icon fa fa-edit fa-3x"></i>
             <div class="info">
-              <h4>Stars</h4>
-              <p><b>500</b></p>
+              <h4>Item Count</h4>
+              <p><b><?=$total_main_stock_item;?></b></p>
             </div>
           </div>
         </div>
       </div>
-      <div class="row">
+<!--       <div class="row">
         <div class="col-md-6">
           <div class="tile">
             <h3 class="tile-title">Monthly Sales</h3>
@@ -68,33 +84,26 @@ include('db_connector.php');
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
       <div class="row">
         <div class="col-md-6">
           <div class="tile">
-            <h3 class="tile-title">Features</h3>
+          
+            <h1 class="tile-title">Features</h1>
             <ul>
-              <li>Built with Bootstrap 4, SASS and PUG.js</li>
-              <li>Fully responsive and modular code</li>
-              <li>Seven pages including login, user profile and print friendly invoice page</li>
-              <li>Smart integration of forgot password on login page</li>
-              <li>Chart.js integration to display responsive charts</li>
-              <li>Widgets to effectively display statistics</li>
-              <li>Data tables with sort, search and paginate functionality</li>
-              <li>Custom form elements like toggle buttons, auto-complete, tags and date-picker</li>
-              <li>A inbuilt toast library for providing meaningful response messages to user's actions</li>
+              <li><h5>Stock Requisition System</h5> </li>
+              <li><h5>Current Items Adding System</h5> </li>
+              <li><h5>Barcode Generation System</h5> </li>
+              <li><h5>Stock Transferring System Between Departments</h5> </li>
+              
             </ul>
-            <p>Vali is a free and responsive admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.</p>
-            <p>Vali is is light-weight, expendable and good looking theme. The theme has all the features required in a dashboard theme but this features are built like plug and play module. Take a look at the <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> about customizing the theme colors and functionality.</p>
-            <p class="mt-4 mb-4"><a class="btn btn-primary mr-2 mb-2" href="http://pratikborsadiya.in/blog/vali-admin" target="_blank"><i class="fa fa-file"></i>Docs</a><a class="btn btn-info mr-2 mb-2" href="https://github.com/pratikborsadiya/vali-admin" target="_blank"><i class="fa fa-github"></i>GitHub</a><a class="btn btn-success mr-2 mb-2" href="https://github.com/pratikborsadiya/vali-admin/archive/master.zip" target="_blank"><i class="fa fa-download"></i>Download</a></p>
+            
           </div>
         </div>
         <div class="col-md-6">
           <div class="tile">
-            <h3 class="tile-title">Compatibility with frameworks</h3>
-            <p>This theme is not built for a specific framework or technology like Angular or React etc. But due to it's modular nature it's very easy to incorporate it into any front-end or back-end framework like Angular, React or Laravel.</p>
-            <p>Go to <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> for more details about integrating this theme with various frameworks.</p>
-            <p>The source code is available on GitHub. If anything is missing or weird please report it as an issue on <a href="https://github.com/pratikborsadiya/vali-admin" target="_blank">GitHub</a>. If you want to contribute to this theme pull requests are always welcome.</p>
+          <h6 class="tile-title" style="color: #CD7F32;" >Click here to visit our webpage...</h6>
+          <a href="http://www.timco.lk/stc/"><img src="upload\logo5.jpg"/></a>
           </div>
         </div>
       </div>
